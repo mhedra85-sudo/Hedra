54f353f525c5b5675405800e07337a2821c94f5c
mhedra85-sudo <mhedra85@gmail.com>
2026-02-13 03:30:42 +0200

    Replace index.html with Hedra v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hedra-v50-pro-1";
+const CACHE = "hedra-v42-pro-1";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js", "./icon.png"];
 
 self.addEventListener("install", (e) => {
