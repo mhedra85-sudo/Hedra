b5f901b9ceb8c308ae02b64601572f10e9af07e8
Hedra <mhedra85@gmail.com>
2026-02-20 10:23:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Hedra V6.2 PRO – Service Worker (cache-first core + offline fallback)
+// Hedra V8 PRO – Service Worker (cache-first core + offline fallback)
 const CACHE="hedra-v6-3-printpro-"+(new Date().toISOString().slice(0,10)).replaceAll("-","");
 
 const SCOPE = self.registration.scope; // e.g. https://user.github.io/Hedra/
